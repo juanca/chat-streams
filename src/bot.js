f16e3495a9b573204ca1f3af6d50a4424ae4061d
Juan Carlos Medina <juanca.med@gmail.com>
2018-09-22 15:58:53 -0700

    Set up WebSocket connection via TMI module.

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,27 @@
+const tmi = require('tmi.js');
+
 module.exports = class Bot {
   constructor(options) {
-    console.log('I am chat streams bot.');
+    let client = new tmi.Client(options);
+
+    client.on('connected', this.onConnectedHandler);
+    client.on('disconnected', this.onDisconnectedHandler);
+    client.on('message', this.onMessageHandler);
+
+    client.connect();
+  }
+
+  onConnectedHandler (addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+  }
+
+  onDisconnectedHandler (reason) {
+    console.log(`Disconnected: ${reason}`);
+  }
+
+  onMessageHandler (target, context, msg, self) {
+    if (self) return;
+
+    console.log(`[${target} (${context['message-type']})] ${context.username}: ${msg}`);
   }
 };
