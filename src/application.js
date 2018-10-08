2384c594a99c1c7040997c3c9b3e986a5b022931
Juan Carlos Medina <juanca.med@gmail.com>
2018-10-08 00:01:45 -0700

    Remove login references in application component

--- src/application.js
+++ src/application.js
@@ -1,32 +1,26 @@
-import Login from './login.js';
 import React from 'react';
 import { connect } from 'react-redux';
 
 function Application(props) {
-  if (props.loggedIn) {
-    return (
-      <React.Fragment>
-        <h1>Chat streams!</h1>
-        <div>Status:
-          <span onClick={props.status === 'Connected' ? props.disconnect : props.connect}>
-            {props.status}
-          </span>
-        </div>
-        <ol>
-          {props.messages.map(message => (console.log('message', message) ||
-            <li>{message.message}</li>
-          ))}
-        </ol>
-      </React.Fragment>
-    );
-  } else {
-    return <Login />;
-  }
+  return (
+    <React.Fragment>
+      <h1>Chat streams!</h1>
+      <div>Status:
+        <span onClick={props.status === 'Connected' ? props.disconnect : props.connect}>
+          {props.status}
+        </span>
+      </div>
+      <ol>
+        {props.messages.map(message => (console.log('message', message) ||
+          <li>{message.message}</li>
+        ))}
+      </ol>
+    </React.Fragment>
+  );
 }
 
 function mapStateToProps(state) {
   return {
-    loggedIn: state.username && state.channels ? true : false,
     status: state.address && state.port && !state.reason ? 'Connected' : 'Disconnected',
     messages: state.messages,
   };
